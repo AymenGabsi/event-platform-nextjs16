e6ce3373ff013afecd6a9df0daaf8428cac6cf4c
Aymen Gabsi <aymengabsi20@gmail.com>
2025-11-02 08:39:40 +0100

    [DevEvent] Project Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 const Home = () => {
-  console.log("What type of component am I ?");
-
-  return <div>Welcome to my Event Platform !</div>;
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event You Can't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One Place
+      </p>
+    </section>
+  );
 };
 
 export default Home;
