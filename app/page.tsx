b01f95052e3e1c0aabde452e802495b60df0e34d
Aymen Gabsi <aymengabsi20@gmail.com>
2025-11-05 11:17:22 +0100

    [Evntix] Fix build errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
-import {IEvent} from "@/database";
+import EventCard from "@/components/EventCard";
+import { IEvent } from "@/database";
+import { cacheLife } from "next/cache";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-const Home = async () => {
+
+const Page = async () => {
+  "use cache";
+  cacheLife("hours");
   const response = await fetch(`${BASE_URL}/api/events`);
   const { events } = await response.json();
 
@@ -35,4 +39,4 @@ const Home = async () => {
   );
 };
 
-export default Home;
+export default Page;
